23d8528dbae82097af7f3f691f22ea0d99c310bf
Renato Ferreira <renatoferreira.dev@gmail.com>
2024-07-02 19:24:21 +0200

    feat(tt-fe): update title to deploy to stage environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-xl lg:flex">
-        <h1>Coming soon...</h1>
+        <h1>Choir on mission...</h1>
       </div>
     </main>
   );
